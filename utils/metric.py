24e99eef815dfb90f861566f8c8d447992c5128c
yangtze7 <shiyangtze@gmail.com>
2021-05-27 15:20:36 +0800

    fix error

--- utils/metric.py
+++ utils/metric.py
@@ -6,12 +6,12 @@ class IOUMetric:
     Class to calculate mean-iou using fast_hist method
     """
 
-    def __init__(self, num_classes=4):
+    def __init__(self, num_classes=5):
         self.num_classes = num_classes
         self.hist = np.zeros((num_classes, num_classes))
 
     def _fast_hist(self, label_pred, label_true):
-        mask = (label_true > 0) & (label_true <= self.num_classes)
+        mask = (label_true >= 0) & (label_true < self.num_classes)
         hist = np.bincount(
             self.num_classes * label_true[mask].astype(int) +
             label_pred[mask], minlength=self.num_classes ** 2).reshape(self.num_classes, self.num_classes)
@@ -23,10 +23,12 @@ class IOUMetric:
 
     def evaluate(self):
         acc = np.diag(self.hist).sum() / self.hist.sum()
+        pe = [self.hist[i,:].sum()*self.hist[:,i].sum() for i in range(self.num_classes)].sum()/(self.hist.sum()*self.hist.sum())
+        kappa = (acc-pe)/(1-pe)
         acc_cls = np.diag(self.hist) / self.hist.sum(axis=1)
         acc_cls = np.nanmean(acc_cls)
         iu = np.diag(self.hist) / (self.hist.sum(axis=1) + self.hist.sum(axis=0) - np.diag(self.hist))
         mean_iu = np.nanmean(iu)
         freq = self.hist.sum(axis=1) / self.hist.sum()
         fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-        return acc, acc_cls, iu, mean_iu, fwavacc
+        return acc, kappa, acc_cls, iu, mean_iu, fwavacc
